4abdeff512175dedeb2415e088972453c83cc308
Julius <juliusarthur2005@gmail.com>
2025-05-16 23:37:02 +0700

    feat: papan class

---
+++ src/Papan.java
@@ -0,0 +1,88 @@
+package Tucil3_13523025_13523030.src;
+
+import java.util.ArrayList;
+
+public class Papan{
+    private int baris;
+    private int kolom;
+    // private ArrayList<Piece> pieces;
+    private char board[][];
+
+    public Papan(int n, int m){
+        this.baris = n;
+        this.kolom = m;
+        // this.pieces = new ArrayList<Piece>();
+        this.board = new char[n][m];
+
+        for (int i = 0; i < baris; i++){
+            for (int j = 0; j < kolom; j++){
+                board[i][j] = '.';
+            }
+        }
+
+    }
+
+    public boolean addPiece(Piece p){
+        int barisPiece = p.getBaris();
+        int kolomPiece = p.getKolom();
+        if (barisPiece >= this.baris || kolomPiece >= this.kolom){
+            System.out.println("Input melebihi board");
+            return false;
+        }
+        else if (p.isHorizontal()){
+            for (int i = kolomPiece; i < p.getPanjang(); i++){
+                if (i >= this.kolom || board[barisPiece][i] != '.'){
+                    System.out.println("Gagal");
+                    return false;
+                }
+            }
+
+            for (int i = kolomPiece; i < p.getPanjang(); i++){
+                this.board[barisPiece][i] = p.getNama();
+            }
+            System.out.println("Piece berhasil ditambahkan");
+            return true;
+        }
+
+        else if (!p.isHorizontal()){
+            for (int i = barisPiece; i < p.getPanjang(); i++){
+                if (i >= this.baris || board[i][kolomPiece] != '.'){
+                    System.out.println("Gagal");
+                    return false;
+                }
+            }
+
+            for (int i = barisPiece; i < p.getPanjang(); i++){
+                this.board[i][kolomPiece] = p.getNama();
+            }
+            System.out.println("Piece berhasil ditambahkan");
+            return true;
+        }
+        return true;
+    }
+
+    public void remove_piece(Piece p){
+        if (p.isHorizontal()){
+            for (int i = p.getKolom(); i < p.getPanjang(); i++){
+                board[p.getBaris()][i] = '.';
+            }
+        }
+
+        if (!p.isHorizontal()){
+            for (int i = p.getBaris(); i < p.getPanjang(); i++){
+                board[i][p.getKolom()] = '.';
+            }
+        }
+
+        // pieces.remove(p);
+    }
+
+    public void displayBoard(){
+        for (int i = 0; i < this.baris; i++){
+            for (int j = 0; j < this.kolom; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
