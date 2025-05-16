0d01195b37a4eb82b7c98978bf508d42bd20ec1d
Julius <juliusarthur2005@gmail.com>
2025-05-16 23:37:18 +0700

    feat: Piece class

---
+++ src/Piece.java
@@ -0,0 +1,41 @@
+package Tucil3_13523025_13523030.src;
+
+public class Piece {
+    private int panjang;
+    private boolean horizontal;
+    private char nama;
+    private int baris, kolom;
+
+    public Piece(int panjang, boolean horizontal, char nama, int baris, int kolom){
+        this.panjang = panjang;
+        this.horizontal = horizontal;
+        this.nama = nama;
+        this.baris = baris;
+        this.kolom = kolom;
+    }
+
+    public boolean isHorizontal(){
+        return horizontal;
+    }
+
+    public int getPanjang(){
+        return panjang;
+    }
+
+    public char getNama(){
+        return nama;
+    }
+
+    public void set_position(int baris, int kolom){
+        this.baris = baris;
+        this.kolom = kolom;
+    }
+
+    public int getBaris(){
+        return this.baris; 
+    }
+
+    public int getKolom(){
+        return kolom;
+    }
+}
