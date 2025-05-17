11600656904a739a31d598452dff0591e83c1dc7
Julius <juliusarthur2005@gmail.com>
2025-05-17 14:49:57 +0700

    feat: testing

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,19 @@ public class Main {
         Piece p2 = new Piece(2, false, 'B', 0, 3);
         Papan board = new Papan(5, 5);
         
-
         board.addPiece(p1);
         board.addPiece(p2);
+
+        board.displayBoard();
+        System.out.println();
+        Gerakan g1 = new Gerakan(p2, "bawah",  1);
+        board.movePiece(g1);
+        board.displayBoard();
+
+        board.displayBoard();
+        System.out.println();
+        Gerakan g2 = new Gerakan(p1, "kanan",  2);
+        board.movePiece(g2);
         board.displayBoard();
     }
 }
