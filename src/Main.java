e2e89511e88b8b3f5e33274746209730a8552f0b
jhotlann <19623263@std.stei.itb.ac.id>
2025-05-18 16:17:03 +0700

    test: greedy test

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,33 @@ package Tucil3_13523025_13523030.src;
 
 public class Main {
     public static void main(String[] args){
-        Piece p1 = new Piece(3, true, 'A', 0, 0);
-        Piece p2 = new Piece(2, false, 'B', 0, 3);
-        Papan board = new Papan(5, 5);
-        
-        board.addPiece(p1);
-        board.addPiece(p2);
+        // Buat papan 6x6
+        Papan papan = new Papan(6, 6);
 
-        board.displayBoard();
-        System.out.println();
-        Gerakan g1 = new Gerakan(p2, "bawah",  1);
-        board.movePiece(g1);
-        board.displayBoard();
+        // Tambahkan beberapa piece
+        Piece primaryPiece = new Piece(2, true, 'X', 2, 1); // panjang 2, horizontal, mulai di (2,1)
+        Piece p1 = new Piece(3, false, 'A', 0, 0);
+        Piece p2 = new Piece(2, true, 'B', 0, 2);
+        Piece p3 = new Piece(2, false, 'C', 3, 5);
+        Piece p4 = new Piece(2, true, 'D', 4, 0);
 
-        board.displayBoard();
-        System.out.println();
-        Gerakan g2 = new Gerakan(p1, "kanan",  2);
-        board.movePiece(g2);
-        board.displayBoard();
+        // Tambahkan ke papan
+        papan.addPiece(primaryPiece);
+        papan.addPiece(p1);
+        papan.addPiece(p2);
+        papan.addPiece(p3);
+        papan.addPiece(p4);
+
+        System.out.println("Board awal:");
+        papan.displayBoard();
+
+        // Tentukan koordinat pintu keluar
+        int exitRow = 2;
+        int exitCol = 5;
+
+        // Jalankan algoritma greedy
+        System.out.println("\nMenjalankan Greedy Best-First Search...");
+        new Greedy(papan, primaryPiece, exitRow, exitCol);
     }
+    
 }
