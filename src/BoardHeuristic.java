c5d1caec31ef897f49038bfe42ea4bbd502518ea
jhotlann <19623263@std.stei.itb.ac.id>
2025-05-19 13:10:12 +0700

    feat:add interface BoardHeuristic

---
+++ src/BoardHeuristic.java
@@ -0,0 +1,15 @@
+package Tucil3_13523025_13523030.src;
+
+/**
+ * Interface for heuristic functions to evaluate board states
+ */
+public interface BoardHeuristic {
+    /**
+     * Calculate the heuristic value for the given board state
+     * Lower values indicate states that are closer to the goal
+     * 
+     * @param state The board state to evaluate
+     * @return The heuristic value
+     */
+    int calculate(Papan state);
+}
