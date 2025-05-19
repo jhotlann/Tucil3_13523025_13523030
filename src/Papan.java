e66faafde156eab56cb672847e1c9bf3d919e978
Julius <juliusarthur2005@gmail.com>
2025-05-19 09:46:51 +0700

    fix: move

--- src/Papan.java
+++ src/Papan.java
@@ -15,8 +15,8 @@ public class Papan{
         this.kolom = m;
         this.pieces = new ArrayList<Piece>();
         this.board = new char[n][m];
-        this.barisExit = barisExit;
-        this.kolomExit = kolomExit;
+        this.exitRow = barisExit;
+        this.exitCol = kolomExit;
 
         for (int i = 0; i < baris; i++){
             for (int j = 0; j < kolom; j++){
@@ -29,7 +29,11 @@ public class Papan{
     public Papan(Papan papan){
         this.baris = papan.baris;
         this.kolom = papan.kolom;
-        this.pieces = new ArrayList<Piece>(papan.pieces);
+        this.pieces = new ArrayList<Piece>();
+
+        for (Piece p : papan.getPieces()){
+            this.pieces.add(new Piece(p));
+        }
         this.board = new char[baris][kolom];
         for (int i = 0; i < baris; i++){
             for (int j = 0; j < kolom; j++){
@@ -47,7 +51,7 @@ public class Papan{
         }
         else if (p.isHorizontal()){
             for (int i = 0; i < p.getPanjang(); i++){
-                if (i >= this.kolom || board[barisPiece][i + kolomPiece] != '.'){
+                if (i + kolomPiece >= this.kolom || (board[barisPiece][i + kolomPiece] != '.')){
                     System.out.println("Gagal");
                     return false;
                 }
@@ -57,14 +61,20 @@ public class Papan{
                 this.board[barisPiece][i + kolomPiece] = p.getNama();
             }
             this.pieces.add(p);
-            System.out.println("Piece berhasil ditambahkan");
+            // System.out.println("Piece berhasil ditambahkan");
+
+            //  if (p.getNama() == 'X'){
+            //     System.out.printf("Barisx: %d  Kolomx: %d\n", barisPiece, kolomPiece);
+            //     displayBoard();
+            //     System.out.println("=========");
+            // }
             return true;
         }
 
         else if (!p.isHorizontal()){
             for (int i = 0; i < p.getPanjang(); i++){
-                if (i >= this.baris || board[i + barisPiece][kolomPiece] != '.'){
-                    System.out.println("Gagal");
+                if (i + barisPiece >= this.baris || (board[i + barisPiece][kolomPiece] != '.')){
+                    // System.out.println("Gagal");
                     return false;
                 }
             }
@@ -73,25 +83,32 @@ public class Papan{
                 this.board[i + barisPiece][kolomPiece] = p.getNama();
             }
             this.pieces.add(p);
-            System.out.println("Piece berhasil ditambahkan");
+            // System.out.println("Piece berhasil ditambahkan");
             return true;
         }
         return true;
     }
 
     public void remove_piece(Piece p){
+        // System.out.printf("Initial remove %c: \n", p.getNama());
+        // displayBoard();
         if (p.isHorizontal()){
-            for (int i = p.getKolom(); i < p.getPanjang(); i++){
-                board[p.getBaris()][i] = '.';
+            if (p.getNama() == 'X'){
+                // System.out.printf("BarisX init: %d, KolomX init: %d\n", p.getBaris(), p.getKolom());
+            }
+            for (int i = 0; i < p.getPanjang(); i++){
+                this.board[p.getBaris()][i + p.getKolom()] = '.';
             }
         }
-
+        
         if (!p.isHorizontal()){
-            for (int i = p.getBaris(); i < p.getPanjang(); i++){
-                board[i][p.getKolom()] = '.';
+            for (int i = 0; i < p.getPanjang(); i++){
+                this.board[i + p.getBaris()][p.getKolom()] = '.';
             }
         }
-
+        
+        // System.out.println("Final remove: ");
+        // displayBoard();
         // pieces.remove(p);
     }
 
@@ -105,13 +122,22 @@ public class Papan{
     }
 
     public boolean movePiece(Gerakan g){
-        Piece p = g.getPiece();
+        // System.out.printf("Initial state move %s %c: \n", g.arah,  g.getPiece().getNama());
+        // System.out.printf("Baris: %d, Kolom: %d \n", g.getPiece().getBaris(), g.getPiece().getKolom());
+        // displayBoard();
+        // System.err.println();
+        Piece p = null;
+        for (Piece i : pieces){
+            if (g.getPiece().getNama()== i.getNama()){
+                p = i;
+            }
+        }
         remove_piece(p);
-
+        
         int oldBaris = p.getBaris();
         int oldKolom = p.getKolom();
-
-        if (g.arah == "kanan" || g.arah == "kiri"){
+        
+        if (g.arah.equals("kanan") || g.arah.equals("kiri")){
             int jarak = g.jumlahKotak;
             if (g.arah == "kiri"){
                 jarak = (-1) * g.jumlahKotak;
@@ -120,22 +146,25 @@ public class Papan{
         }
         else{
             int jarak = g.jumlahKotak;
-
+            
             if (g.arah == "atas"){
-                jarak *= (-1) * g.jumlahKotak;
+                jarak = (-1) * g.jumlahKotak;
             }
             p.set_position(oldBaris + jarak, oldKolom);
         }
         
         boolean add = addPiece(p);
-
+        // System.out.printf("Final state move %c: \n", p.getNama());
+        // displayBoard();
+        // System.err.println();
+        
         if (add){
-            System.out.println("Berhasil pindah");
+            // System.out.println("Berhasil pindah");
             return true;
         }
         
         else{
-            System.out.println("Gagal pindah");
+            // System.out.println("Gagal pindah");
             p.set_position(oldBaris, oldKolom);
             addPiece(p);
             return false;
@@ -185,5 +214,15 @@ public class Papan{
         return sb.toString();
     }
 
+    public Piece getprimaryPiece(){
+        for (Piece p : pieces){
+            if (p.isPrimary()){
+                return p;
+            }
+        }
+        
+        return null;
+    }
 
+    
 }
