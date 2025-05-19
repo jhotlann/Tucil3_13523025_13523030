933ca0d29530b9a2195cd1de2417ed248351d4ea
jhotlann <19623263@std.stei.itb.ac.id>
2025-05-19 13:09:54 +0700

    feat: add node class

---
+++ src/Node.java
@@ -0,0 +1,50 @@
+package Tucil3_13523025_13523030.src;
+
+/**
+ * Node class for search algorithm
+ * Represents a state in the search tree
+ */
+public class Node {
+    private Papan state;
+    private Node parent;
+    private Gerakan action;
+    private int heuristic;
+    
+    /**
+     * Create a new node with the given state, parent, action, and heuristic value
+     */
+    public Node(Papan state, Node parent, Gerakan action, int heuristic) {
+        this.state = state;
+        this.parent = parent;
+        this.action = action;
+        this.heuristic = heuristic;
+    }
+    
+    /**
+     * Get the state represented by this node
+     */
+    public Papan getState() {
+        return state;
+    }
+    
+    /**
+     * Get the parent node
+     */
+    public Node getParent() {
+        return parent;
+    }
+    
+    /**
+     * Get the action that led to this node from its parent
+     */
+    public Gerakan getAction() {
+        return action;
+    }
+    
+    /**
+     * Get the heuristic value of this node
+     */
+    public int getHeuristic() {
+        return heuristic;
+    }
+}
